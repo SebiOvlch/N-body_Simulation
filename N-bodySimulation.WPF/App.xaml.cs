77500e4d749e89772513fb09458abf8db10c6f7c
Ovlachi Sebestyen <Ovlachi Sebestyen@DESKTOP-8J248NM>
2025-11-12 11:20:50 +0200

    Add project files.

---
+++ N-bodySimulation.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace N_bodySimulation.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
